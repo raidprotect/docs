23f2c6d7d96f31511729a500fd0667651fccf87e
Zallom <matthias@fca.gg>
2025-07-09 19:11:08 +0200

    fix: BrowserWindow baseUrml

--- src/components/BrowserWindow/index.tsx
+++ src/components/BrowserWindow/index.tsx
@@ -21,7 +21,7 @@ interface Props {
 export default function BrowserWindow({
   children,
   minHeight,
-  url = 'http://localhost.com:3000',
+  url = 'http://localhost:3000',
   style,
   bodyStyle,
 }: Props): ReactNode {
